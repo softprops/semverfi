2d382029e5437b8b81badfe4bc37b5d5a07f05a9
softprops <d.tangren@gmail.com>
2012-08-23 01:20:40 -0400

    improving

--- src/main/scala/parse.scala
+++ src/main/scala/parse.scala
@@ -1,8 +1,8 @@
 package semverfi
 
-import scala.util.parsing.combinator.JavaTokenParsers
+import scala.util.parsing.combinator.RegexParsers
 
-object Parse extends JavaTokenParsers {
+object Parse extends RegexParsers {
 
   private val Dot = "."
 
@@ -10,57 +10,53 @@ object Parse extends JavaTokenParsers {
 
   private val Dash = "-"
 
-  def id: Parser[String] = """[\w|\d]+""".r
+  def id: Parser[String] = """[0-9A-Za-z-]+""".r
 
   def int: Parser[String] = """\d+""".r
 
-  def any: Parser[String] = """.""".r
+  def any: Parser[String] = """.+""".r
 
   def version: Parser[SemVersion] =
-    buildVersion | preReleaseVersion | normalVersion | invalidVersion
+    validVersion | log(invalidVersion)("inv version")
+
+  def validVersion: Parser[SemVersion] =    
+    buildVersion | preReleaseVersion | normalVersion
 
   def buildVersion: Parser[Build.Version] =
-    versionTuple ~ classifier.? ~ Plus ~ rep(id | id <~ Dot) ^^ {
-      case ((maj, min, pat) ~ maybeClassifier ~ _ ~ ids) =>
-        println("parsed build")
-        Build.Version(maj, min, pat,
-                      ids ++ maybeClassifier.getOrElse(Nil))
+    versionTuple ~ classifier.? ~ (Plus ~> classifierSegs) ^^ {
+      case ((major, minor, patch) ~ maybeClassifier ~ ids) =>
+        Build.Version(major, minor, patch,
+                      maybeClassifier.getOrElse(Nil) ++ ids)
     }
 
   def preReleaseVersion: Parser[PreRelease.Version] =
     versionTuple ~ classifier ^^ {
-      case ((maj, min, pat) ~ classifier) =>
-        println("parsed pre-release")
-        PreRelease.Version(maj, min, pat, classifier)
+      case ((major, minor, patch) ~ classifier) =>
+        PreRelease.Version(major, minor, patch, classifier)
     }
 
   def invalidVersion: Parser[Invalid] =
-    rep(any) ^^ {
-      case chars =>
-        println("parsed invalid")
-        Invalid(chars.mkString(""))
+    any.* ^^ {
+      case cs => Invalid(cs mkString(""))
     }
 
   def normalVersion: Parser[Normal.Version] =
     versionTuple ^^ {
       case (major, minor, patch) =>
-        println("parsed normal")
         Normal.Version(major, minor, patch)
     }
 
   def versionTuple: Parser[(Int, Int, Int)] =
-    (int ~ Dot ~ int ~ Dot ~ int) ^^ {
-      case (maj ~ _ ~ min ~ _ ~ pat) =>
-        println("parsed version tuple")
+    int ~ (Dot ~> int) ~ (Dot ~> int) ^^ {
+      case (maj ~ min ~ pat) =>
         (maj.toInt, min.toInt, pat.toInt)
   }
 
+  def classifierSegs: Parser[Seq[String]] =
+    rep1(id | Dot ~> id)
+
   def classifier: Parser[Seq[String]] =
-    Dash ~ rep(id | Dot ~> id) ^^ {
-      case (_ ~ ids) =>
-        println("parsed classifier")
-        ids
-    }
+    Dash ~> classifierSegs
 
   def apply(in: String): ParseResult[SemVersion] =
     parseAll(version, in)
