6f8c88a25fef8691be3fed034d30df89da23fa52
softprops <d.tangren@gmail.com>
2013-03-20 23:27:31 -0400

    opt method

--- src/main/scala/semver.scala
+++ src/main/scala/semver.scala
@@ -1,13 +1,15 @@
 package semverfi
 
 object Version {
-  def apply(in: String): SemVersion = Parse(in)
+  def apply(in: String) = Parse(in)
 }
 
+/** these get put at the bottom of ordered results */
 sealed trait SemVersion extends SemVersionOrdering {
   def major: Int
   def minor: Int
   def patch: Int
+  def opt: Option[Valid]
   override def toString = Show(this)
 }
 
@@ -15,11 +17,15 @@ case class Invalid(raw: String) extends SemVersion {
   def major = -1
   def minor = -1
   def patch = -1
+  def opt = None
 }
 
 sealed trait Valid extends SemVersion
-        with Bumping
         with Appending
+        with Bumping
+        with Normalizing {
+  def opt = Some(this)
+}
 
 case class NormalVersion(major: Int, minor: Int, patch: Int)
    extends Valid
