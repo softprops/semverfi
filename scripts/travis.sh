14e6e1097df99a07a0b524f707d8c2773d3b26e0
softprops <d.tangren@gmail.com>
2013-03-24 14:09:10 -0400

    need to know we're running in a travis context

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # thank @xuwei-k
+export TRAVIS=true &&
 curl -O https://raw.github.com/paulp/sbt-extras/1dbab99024e2bec49b9171b61eac4d48654eaf26/sbt &&
 chmod u+x ./sbt &&
 ./sbt -mem 512 +test
